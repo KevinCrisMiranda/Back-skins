3da4319c85f96498b557aab675597279f50db090
KevinMiranda75 <kevin.miranda@lexis.com.ec>
2024-05-05 11:50:35 -0500

    quitando base de datos 2

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,15 +1,24 @@
+// module.exports = {
+//     database:{
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'copy'
+//     }
+//     // database:{
+//     //     host: 'localhost',
+//     //     user: 'root',
+//     //     password: 'V1ramses$',
+//     //     database: 'abet'
+//     // }
+
+// }
+require("dotenv").config()
 module.exports = {
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'copy'
+        host:  process.env.DB,
+        user:  process.env.USER,
+        password: process.env.PASSWORD, 
+        database:  process.env.DATABASE 
     }
-    // database:{
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: 'V1ramses$',
-    //     database: 'abet'
-    // }
-
-}
+}      
